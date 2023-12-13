d48a11a002724298d601c93ce5bfa50721ef323d
hkirvesl <43118971+hkirvesl@users.noreply.github.com>
2023-12-13 12:44:37 +0100

    add some slurm scripts

---
+++ slurm_stuff/ect_slurm_tools.py
@@ -0,0 +1,20 @@
+from numpy import genfromtxt
+import numpy as np
+import ????
+
+def compute_ECT(infile,outfile):
+    s1=Shapereader(infile)
+    ECT=ECT_MC(s1)
+    np.save(outfile,ECT)
+
+def compute_ECT_MC(shape,p=100)
+    directions = genfromtxt('directions326.csv', delimiter=',')
+    n=directions.shape[0]
+    heights=np.linspace(-1,1,p)
+    ECT=np.zeros([n,p])
+    for i in range(n):
+        direction=directions[i,:]
+        for j in range(p):
+            height=heights[j]
+            ECT[i,j]=EC(heights[j]*n # TODO: Fix
+    return(ECT)
---
+++ slurm_stuff/sbatch.q
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --array=1-38%5
+#SBATCH --nodes=1
+#SBATCH --mem=10G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=logs/MC/runs/run-%a.out
+#SBATCH --error=logs/MC/errors/errors-%a.err
+
+#load conda and activate the desired environment
+# (the directory here is just an example, won't work on EPFL systems
+# (and these 2 lines may not even be needed)
+module load gcc
+module load python
+#conda activate /data/mukherjeelab/envs/conda
+# fire off python
+python my_script.py $SLURM_ARRAY_TASK_ID 
+
